2a4225fef6a4e034f54389ebfd52fc3d1f6eab49
Michael Jaoudi <mjaoudi93@gmail.com>
2011-07-06 22:49:02 -0400

    Added file for snake body

---
+++ snake/snake/SnakeBody.h
@@ -0,0 +1,18 @@
+//
+//  SnakeBody.h
+//  snake
+//
+//  Created by pH on 7/6/11.
+//  Copyright 2011 http://ortatherox.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+
+@interface SnakeBody : NSObject {
+    SnakeBody *nextSnake;
+    
+}
+
+@end
---
+++ snake/snake/SnakeBody.m
@@ -0,0 +1,14 @@
+//
+//  SnakeBody.m
+//  snake
+//
+//  Created by pH on 7/6/11.
+//  Copyright 2011 http://ortatherox.com. All rights reserved.
+//
+
+#import "SnakeBody.h"
+
+
+@implementation SnakeBody
+
+@end
