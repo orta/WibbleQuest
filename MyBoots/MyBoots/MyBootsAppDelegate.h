0b14d3d52a39a59a7bc989438a3a9d96442d9978
orta <orta.therox@gmail.com>
2011-07-17 13:25:43 -0400

    doesnt use ARC for retaining appdelegate properties

--- MyBoots/MyBoots/MyBootsAppDelegate.h
+++ MyBoots/MyBoots/MyBootsAppDelegate.h
@@ -12,8 +12,8 @@
 
 @interface MyBootsAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (/*strong, nonatomic*/) UIWindow *window;
+@property (retain, nonatomic) UIWindow *window;
 
-@property (/*strong, nonatomic*/) GameViewController *mainViewController;
+@property (retain, nonatomic) GameViewController *mainViewController;
 
 @end
