39619f3a952b15d7d07211f9370416b599a36ca2
orta <orta.therox@gmail.com>
2011-08-01 20:19:54 -0400

    moved game logic into game folders and everything else out|

---
+++ WibbleQuest/GameSelector.m
@@ -0,0 +1,18 @@
+//
+//  GameSelector.m
+//  WibbleQuest
+//
+//  Created by orta therox on 01/08/2011.
+//  Copyright 2011 http://ortatherox.com. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface GameSelector : NSObject @end
+
+@implementation GameSelector
+
+- (void)awakeFromNib {
+  
+}
+
+@end
