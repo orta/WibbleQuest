06d6f1ab55f7fae57088cfb94cd7762cb8d4a9d6
Michael Jaoudi <mjaoudi93@gmail.com>
2011-08-02 13:50:11 -0400

    Updated Creature, added in order attack phrases and removed need for %i, Updated TODO

---
+++ tests/creature/CreatureTests.h
@@ -0,0 +1,13 @@
+//
+//  CreatureTests.h
+//  WibbleQuest
+//
+//  Created by Michael Jaoudi on 7/31/11.
+//  Copyright 2011 http://ortatherox.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CreatureTests : NSObject
+
+@end
---
+++ tests/creature/CreatureTests.m
@@ -0,0 +1,23 @@
+//
+//  CreatureTests.m
+//  WibbleQuest
+//
+//  Created by Michael Jaoudi on 7/31/11.
+//  Copyright 2011 http://ortatherox.com. All rights reserved.
+//
+
+#import "CreatureTests.h"
+
+@implementation CreatureTests
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
