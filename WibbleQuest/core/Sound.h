db54fa907cb4d0d3157daf7a091d7897716c9b7f
Michael Jaoudi <mjaoudi93@gmail.com>
2011-08-17 23:45:36 -0400

    Added Sound API

---
+++ WibbleQuest/core/Sound.h
@@ -0,0 +1,16 @@
+//
+//  Sound.h
+//  WibbleQuest
+//
+//  Created by Michael Jaoudi on 8/17/11.
+//  Copyright 2011 http://ortatherox.com. Licensed under Creative Commons
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface Sound : NSObject
+
++(void)play:(NSString*)file;
+
+@end
