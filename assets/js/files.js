337817c05d766938e78e099edb4c77a3401b06f3
orta <orta.therox@gmail.com>
2011-08-31 17:58:43 +0300

    updated API to new site layout

---
+++ assets/js/files.js
@@ -0,0 +1,2 @@
+var classes = ["Inventory","Player","Creature","Item","Person","Room","Store","WibbleObject", "WQ"];
+var core_classes = ["CommandInterpreter","PlayerInventory", "Sound","Walkthrough","WibbleQuest","Game"];
