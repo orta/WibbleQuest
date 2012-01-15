7cf47733543bd23e2acfb4f9937d0d2642d88596
orta <orta.therox@gmail.com>
2012-01-15 13:33:15 -0700

    renamed to LibWibble

---
+++ WibbleQuest/core/LibWibble.h
@@ -0,0 +1,32 @@
+//
+//  LibWibble.h
+//  WibbleQuest
+//
+//  Created by orta therox on 15/01/2012.
+//  Copyright (c) 2012 http://ortatherox.com. All rights reserved.
+//
+
+
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import "WibbleQuest.h"
+#import "WQ.h"
+#import "WibbleObject.h"
+
+#import "NSArray-Contains.h"
+#import "NSArray-named-captures.h"
+
+#import "Creature.h"
+#import "Game.h"
+#import "Inventory.h"
+#import "Item.h"
+#import "Person.h"
+#import "Player.h"
+#import "Room.h"
+#import "Store.h"
+#import "Sound.h"
+
+#endif
+
